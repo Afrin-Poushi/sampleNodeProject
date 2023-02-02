f9b696a5946ae7f261bd5b5292898acfeca45be1
Poushi <afrinsultana@iut-dhaka.edu>
2023-02-02 12:29:05 +0600

    newx ocmmit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 8000;
+
 app.get("/", (req, res) => {
   res.send("This is Azure");
 });
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("Listening on port 8000...");
 });
